2f74dbf9dd9301d5755fc5e36296ce108a5d4904
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-20 23:38:43 -0300

    feat(ui): create `TodoItem` component

---
+++ src/routes/TodoItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let todo: Todo;
+</script>
+
+<div class="flex justify-between gap-10">
+	<label
+		data-is-completed={todo.completed}
+		for={todo.id}
+		class="data-[is-completed='true']:line-through data-[is-completed='true']:opacity-50"
+	>
+		{todo.content}
+	</label>
+	<input
+		class="p-2 text-xl cursor-pointer text-accent focus:ring-accent focus:border-white bg-foreground focus:ring-offset-0"
+		type="checkbox"
+		id={todo.id}
+		bind:checked={todo.completed}
+	/>
+</div>
