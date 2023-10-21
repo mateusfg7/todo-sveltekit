96d13562bcbf5f69dcd8082049a866ea7abc51e1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-20 23:37:28 -0300

    chore(tailwind): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{html,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				background: '#1a1a1a',
+				foreground: '#2E2E2E',
+				accent: '#FF3E00'
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/forms')]
 };
