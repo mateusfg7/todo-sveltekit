9f0a4067d9b9818dab2a0648f56e2bee1a9c2308
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-22 10:06:35 -0300

    feat(ui): add "delete" button

--- src/lib/components/TodoItem.svelte
+++ src/lib/components/TodoItem.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
+	import Trash from 'phosphor-svelte/lib/Trash';
+
 	export let todo: Todo;
+	export let removeTodo: (id: string) => void;
 </script>
 
-<div class="flex justify-between gap-10">
-	<label
-		data-is-completed={todo.completed}
-		for={todo.id}
-		class="data-[is-completed='true']:line-through data-[is-completed='true']:opacity-50"
-	>
-		{todo.content}
+<div class="flex items-center justify-between w-full gap-2">
+	<label for={todo.id} class="flex items-center w-full gap-10 p-1 overflow-hidden cursor-pointer">
+		<input
+			class="w-6 h-6 text-xl cursor-pointer text-accent focus:border-transparent bg-foreground/60 hover:bg-foreground focus:ring-0 focus:ring-offset-0 focus:outline-1 focus:outline-offset-2 focus:outline-accent"
+			type="checkbox"
+			aria-details="Complete to-do"
+			id={todo.id}
+			bind:checked={todo.completed}
+		/>
+		<p
+			data-is-completed={todo.completed}
+			title={todo.content}
+			class="data-[is-completed='true']:line-through text-ellipsis w-full overflow-hidden text-lg leading-none cursor-pointer data-[is-completed='true']:opacity-50"
+		>
+			{todo.content}
+		</p>
 	</label>
-	<input
-		class="p-2 text-xl cursor-pointer text-accent focus:ring-accent focus:border-white bg-foreground focus:ring-offset-0"
-		type="checkbox"
-		id={todo.id}
-		bind:checked={todo.completed}
-	/>
+	<button class="text-neutral-600/30 hover:text-red-600" on:click={() => removeTodo(todo.id)}>
+		<Trash size={25} weight="duotone" />
+	</button>
 </div>
